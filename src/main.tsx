a02452c7f4779e39d8d4299594956a493f3c6e2c
Kaetaeru <131461278+Kaetaeru@users.noreply.github.com>
2026-08-13 21:30:51 +0900

    ui: wire v0.9 character creation adapter

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import { AppProvider } from "./app/AppProvider";
+import "./app/characterCreationV09";
 import "./app/mockAdapterCompletion";
 import "./styles.css";
 import "./responsive.css";
 import "./completion.css";
+import "./character-creation-v09.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
