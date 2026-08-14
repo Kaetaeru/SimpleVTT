6dff90fda599bee8934aad6ffc960c09ea5c5efc
Kaetaeru <131461278+Kaetaeru@users.noreply.github.com>
2026-08-15 06:03:13 +0900

    app: load subclass runtime adapter

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import "./app/sorceryRuntimeAdapter";
 import "./app/progressionPhase08SorcererAdapter";
 import "./app/progressionPhase08WarlockAdapter";
 import "./app/progressionPersistentFeatureRuntimeAdapter";
+import "./app/subclassRuntimeAdapter";
 import "./app/classFeatureSpellRuntimeAdapter";
 import { CombatSpellHudBridge } from "./CombatSpellHud";
 import { LevelUpV10Bridge } from "./LevelUpV10";
