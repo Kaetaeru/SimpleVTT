a377fb70716156bc87ed8e7c29cff6dae459c4aa
Kaetaeru <131461278+Kaetaeru@users.noreply.github.com>
2026-08-16 00:11:58 +0900

    app: capture legacy spell resolve and undo pair

--- src/app/legacySpellRuntimeHandler.ts
+++ src/app/legacySpellRuntimeHandler.ts
@@ -2,3 +2,4 @@ import "./spellcastingRuntimeAdapter";
 import { MockAdapter } from "./mockAdapter";
 
 export const legacySpellResolveAction = MockAdapter.prototype.resolveAction;
+export const legacySpellUndoLastResolution = MockAdapter.prototype.undoLastResolution;
