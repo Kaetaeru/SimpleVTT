ead9bfd211975957fe65c7c8d90268f32b912e4d
Kaetaeru <131461278+Kaetaeru@users.noreply.github.com>
2026-08-16 23:08:28 +0900

    phase13: restore projected resolution context before rehosting

--- src/app/connectedProjectionLifecycleAdapter.ts
+++ src/app/connectedProjectionLifecycleAdapter.ts
@@ -1,10 +1,16 @@
 import "./connectedSessionRuntimeAdapter";
 import { MockAdapter } from "./mockAdapter";
 import { unmountAllReconstructedCharacterSessionProjections } from "./characterSessionProjectionMount";
+import { isEphemeralSessionProjectionCharacter } from "./characterSessionProjectionRegistry";
+import type { CharacterSheet } from "./contracts";
 
 const previousHostSession=MockAdapter.prototype.hostSession;
 
 MockAdapter.prototype.hostSession=async function hostSessionWithProjectionCleanup() {
+  const app=this as unknown as {activeCharacter:CharacterSheet};
+  if (isEphemeralSessionProjectionCharacter(this,app.activeCharacter.id)) {
+    await this.dismissResolution();
+  }
   unmountAllReconstructedCharacterSessionProjections(this);
   return previousHostSession.call(this);
 };
